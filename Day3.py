c8b8395fda64900e176aca7e05b4a2001e006333
Ross Tinsley <rtinsley1199@gmail.com>
2020-12-03 14:51:31 +0000

    Day 3

---
+++ Day3.py
@@ -0,0 +1,36 @@
+from input_reader import *
+import numpy as np
+
+data = read_task_input(3).split("\n")
+
+STEPS = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+
+x, y = 0, 0
+
+tree_counts = []
+
+for step in STEPS:
+    tree_count = 0
+    
+    at_end = False
+    x, y = 0, 0
+
+    while not at_end:
+        current = data[y][x]
+
+        if current == "#":
+            tree_count += 1
+
+        x += step[0]
+        y += step[1]
+
+        x = x % len(data[0])
+
+        at_end = y > (len(data) - 1)
+
+    tree_counts.append(tree_count)
+
+result = 1
+for t in tree_counts:
+    result *= t
+print(result)
